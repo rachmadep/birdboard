f1d7940eb62a0609a77b1add4b9836a96494ccce
rachmadep <rachmadekopribadi@gmail.com>
2020-05-25 11:17:38 +0700

    Testing Request Validation

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -17,9 +17,12 @@ class ProjectController extends Controller
 
     public function store(Request $request)
     {
-        Project::create(request([
-            'title', 'description'
-        ]));
+        $attributes = request()->validate([
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+
+        Project::create($attributes);
 
         return redirect('/projects');
     }
