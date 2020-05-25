e20c1ea4fd28c81a9804252cec600309e325876d
rachmadep <rachmadekopribadi@gmail.com>
2020-05-26 06:51:26 +0700

    Prepping the Frontend

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,23 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Biardboard</h1>
+@extends('layouts.app')
 
-    <ul>
-        @forelse ($projects as $project)
-            <li>
-                <a href="{{ $project->path() }}">{{ $project->title }}</a>
-            </li>
-        @empty
-            <li>No Projects yet.</li>
-        @endforelse
-    </ul>
+@section('content')
+<div class="flex item-center">
+    <a href="/projects/create">New Project</a>
+</div>
 
-</body>
-</html>
+<ul>
+    @forelse ($projects as $project)
+        <li>
+            <a href="{{ $project->path() }}">{{ $project->title }}</a>
+        </li>
+    @empty
+        <li>No Projects yet.</li>
+    @endforelse
+</ul>
+@endsection
