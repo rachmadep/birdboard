b6790eed3be0dc4365272f1089dd57d559adc20f
rachmadep <rachmadekopribadi@gmail.com>
2020-05-27 06:54:34 +0700

    Touch It

--- app/Task.php
+++ app/Task.php
@@ -8,6 +8,13 @@ class Task extends Model
 {
     protected $guarded = [];
 
+    /**
+     * The relationships that should be touched on save.
+     *
+     * @var array
+     */
+    protected $touches = ['project'];
+
     public function project()
     {
         return $this->belongsTo(Project::class);
