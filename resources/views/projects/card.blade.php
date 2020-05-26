45e00d6cde5672f65a781a9364379b3a17f70a23
rachmadep <rachmadekopribadi@gmail.com>
2020-05-26 14:02:19 +0700

    Stayling the Project Page

---
+++ resources/views/projects/card.blade.php
@@ -0,0 +1,8 @@
+<div class="card" style="height: 200px">
+    <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-200 pl-4">
+        <a href="{{ $project->path() }}" class="text-default">{{ $project->title }}</a>
+    </h3>
+
+    <div class="mb-4 text-gray-500 flex-1">{{ str_limit($project->description) }}</div>
+
+</div>
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,8 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{ $project->title }}</h1>
-    <div>{{ $project->description }}</div>
+<header class="flex item-end mb-3 py-4">
+    <div class="flex justify-between w-full">
+        <p class="text-gray-600 text-lg"><a href="/projects">My Projects</a> / {{ $project->title }}</p>
+    </div>
+</header>
 
-    <a href="/projects">Go Back</a>
+
+<main class="lg:flex lg:flex-wrap -mx-3">
+    <div class="lg:w-3/4 px-3 mb-6">
+        <div class="mb-6">
+            <h2 class="text-lg text-gray-600 font-normal">Task</h2>
+            <div class="card mb-3">Lorem ipsum</div>
+            <div class="card mb-3">Lorem ipsum</div>
+            <div class="card mb-3">Lorem ipsum</div>
+            <div class="card">Lorem ipsum</div>
+        </div>
+
+        <div>
+            <h2 class="text-lg text-gray-600 font-normal">General Notes</h2>
+            <textarea class="card w-full" style="min-height: 200px">Lorem ipsum</textarea>
+        </div>
+    </div>
+    <div class="lg:w-1/4 px-3">
+        @include('projects.card')
+    </div>
+</main>
 @endsection
