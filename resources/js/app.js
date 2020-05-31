9c62643931b3272c76f02fa4b2e3c9fcb322ffbe
rachmadep <rachmadekopribadi@gmail.com>
2020-05-31 13:01:32 +0700

    The New Project Modal: Behavior

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,8 @@ import VModal from 'vue-js-modal'
 Vue.use(VModal)
 
 Vue.component('theme-switcher', require('./components/ThemeSwitcher.vue').default);
+Vue.component('new-project-modal', require('./components/NewProjectModal.vue').default);
+
 const app = new Vue({
     el: '#app',
 });
